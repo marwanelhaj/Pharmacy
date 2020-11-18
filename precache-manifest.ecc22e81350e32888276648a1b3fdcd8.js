7c38ff63e90d5a7fde397f523724cec1132403b2
marwanelhaj <roxelhaj@gmail.com>
2020-11-18 14:34:34 +0100

    Updates

---
+++ precache-manifest.ecc22e81350e32888276648a1b3fdcd8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ac704ab590de38bdc9d1cdab81c23d",
+    "url": "/Pharmacy/index.html"
+  },
+  {
+    "revision": "b58f288ff3a1a2dcbec6",
+    "url": "/Pharmacy/static/css/2.589e1136.chunk.css"
+  },
+  {
+    "revision": "b7ab653bedf62d5bfa9e",
+    "url": "/Pharmacy/static/css/main.4bd90883.chunk.css"
+  },
+  {
+    "revision": "b58f288ff3a1a2dcbec6",
+    "url": "/Pharmacy/static/js/2.77c58773.chunk.js"
+  },
+  {
+    "revision": "66bf4758db04c3a7f24ce071c6127a88",
+    "url": "/Pharmacy/static/js/2.77c58773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9935b914ec113bad164d",
+    "url": "/Pharmacy/static/js/3.8c57631a.chunk.js"
+  },
+  {
+    "revision": "b7ab653bedf62d5bfa9e",
+    "url": "/Pharmacy/static/js/main.e640b675.chunk.js"
+  },
+  {
+    "revision": "10fd87ae67ee26bf34f2",
+    "url": "/Pharmacy/static/js/runtime-main.ae807304.js"
+  },
+  {
+    "revision": "6889e5aebd4d5ed4d7616ae36128cc07",
+    "url": "/Pharmacy/static/media/attach.6889e5ae.svg"
+  },
+  {
+    "revision": "f942b759e9dbc96a15ab7d530ac4552a",
+    "url": "/Pharmacy/static/media/bg14.f942b759.png"
+  },
+  {
+    "revision": "ed3ad8f0e80787d9f497de5da960a49e",
+    "url": "/Pharmacy/static/media/delete.ed3ad8f0.svg"
+  },
+  {
+    "revision": "f03afd7d31370af04a779671510caece",
+    "url": "/Pharmacy/static/media/edit.f03afd7d.svg"
+  },
+  {
+    "revision": "94dd0c22a0eafc711c6a6e3af30e1757",
+    "url": "/Pharmacy/static/media/eye.94dd0c22.svg"
+  },
+  {
+    "revision": "7261fb01f3fb28da27e6851fa7ffff8e",
+    "url": "/Pharmacy/static/media/icon-shape2.7261fb01.svg"
+  },
+  {
+    "revision": "1400df96f6efa1e87885cc6da863d14e",
+    "url": "/Pharmacy/static/media/loading_spinner.1400df96.gif"
+  },
+  {
+    "revision": "3895239d2ece0f294d1ee1557aea73f9",
+    "url": "/Pharmacy/static/media/main-bg.3895239d.jpg"
+  },
+  {
+    "revision": "881acc840c9f5adecdf336fc03d35bba",
+    "url": "/Pharmacy/static/media/overly-bg.881acc84.png"
+  },
+  {
+    "revision": "6973991220b3caf338e55dd309d0a136",
+    "url": "/Pharmacy/static/media/paypal.69739912.png"
+  },
+  {
+    "revision": "c04d98c5eeba5433a95bbdbc1f654252",
+    "url": "/Pharmacy/static/media/pencil.c04d98c5.svg"
+  },
+  {
+    "revision": "71131d35e8adcd4922ca605fb30a27a7",
+    "url": "/Pharmacy/static/media/send-message.71131d35.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Pharmacy/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Pharmacy/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Pharmacy/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Pharmacy/static/media/slick.f97e3bbf.svg"
+  }
+]);
